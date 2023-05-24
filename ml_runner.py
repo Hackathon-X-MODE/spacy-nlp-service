8ffeffcb55aac0c016f34bac7fc879a949815198
Alexander Chapchuk <BigTows@yandex.ru>
2023-05-24 23:53:54 +0300

    Added new models.

--- ml_runner.py
+++ ml_runner.py
@@ -1,7 +1,5 @@
 from ml.model.model import REF, MAIN_TYPE_MODEL, PREPARE_ORDER_SUB_TYPE_MODEL
 
-if (0):
-    REF[MAIN_TYPE_MODEL].traning();
-
-if (1):
-    REF[PREPARE_ORDER_SUB_TYPE_MODEL].traning();
+for model in REF:
+    print("Runner... ", model)
+    REF[model].traning()
